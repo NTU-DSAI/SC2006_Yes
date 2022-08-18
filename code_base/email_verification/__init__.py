34a9d30e69130fc6f75cc95ccb4f0cdc25cbcf28
lohzhishen <82319546+lohzhishen@users.noreply.github.com>
2022-08-18 19:45:36 +0800

    Update __init__.py
    Updated to reflect changes made to otp.py

--- code_base/email_verification/__init__.py
+++ code_base/email_verification/__init__.py
@@ -1,14 +1,14 @@
 '''This package contains modules relate to email verification.
 
 Usage:
-from email_verification.otp import TimedRandomDigitOTP, RandomDigitOTP
+from email_verification.otp import TimedOTP, RandomDigitOTP
 from email_verification.mailer import GmailMailer
 
 recipient = <<fill this in>>
 subject = <<fill this in>>
 contents = <<fill this in>>
 
-OTP_METHOD = TimedRandomDigitOTP # RandomDigitOTP works as well
+OTP_METHOD = lambda x: TimedOTP(RandomDigitOTP(**x))
 MAILER_METHOD = GmailMailer
 
 # parse configs from .json file if needed
@@ -27,11 +27,11 @@ otp_gen = OTP_METHOD(**otp_config)
 mailer = MAILER_METHOD(**mailer_config)
 
 # run
-mailer.send(recipient = recipient, subject = subject, contents = contents.format(otp_gen.otp))
+mailer.send(recipient = recipient, subject = subject, contents = contents.format(otp.generate()))
 value = input("Enter your OTP: ")
-print("Pass" if otp_gen.verify(value) else 'Fail')
+print("Pass" if otp.verify(value) else 'Fail')
 '''
 
 __author__ = 'Loh Zhi Shen'
-__verison__ = '1.0.0'
+__verison__ = '1.0.1'
 __last_updated__ = '18/08/2022'
