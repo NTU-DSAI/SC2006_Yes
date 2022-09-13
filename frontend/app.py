bf498a8a120994a4918df10a5d482788e2a31c37
lohzhishen <82319546+lohzhishen@users.noreply.github.com>
2022-09-13 17:15:09 +0800

    Added a dev bar to all pages and basic css styling for the dev bar

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return "Hello, World!"
+   return render_template("landingpage.html")
+
+@app.route('/admin')
+def admin():
+   return render_template("adminpage.html")
+
+@app.route('/information')
+def information():
+   return render_template("informationpage.html")
+
+@app.route('/landing')
+def landing():
+   return render_template("landingpage.html")
+
+@app.route('/location')
+def location():
+   return render_template("locationpage.html")
+
+@app.route('/login')
+def login():
+   return render_template("loginpage.html")
+
+@app.route('/recommend')
+def recommend():
+   return render_template("recommendpage.html")
+
+@app.route('/route')
+def route():
+   return render_template("routepage.html")
+
+@app.route('/selection')
+def selection():
+   return render_template("selectionpage.html")
+
+@app.route('/suggestion')
+def suggestion():
+   return render_template("suggestionpage.html")
   
 if __name__ == "__main__":
   app.run(debug = True)
